cf0a41cb89ce4217a9a231d6a2456ba85e1b4c26
JHON ZAPATA <triangelturbo@gmail.com>
2023-05-17 08:32:12 -0500

    Actualización de archivos: actionButton.html, js/main.js, navbar.html, plantilla.html

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
 
-       document.addEventListener('DOMContentLoaded', function() {
-        M.AutoInit();
-       });
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+
+  let sidenav = document.querySelectorAll('.sidenav');
+  let instancia_sidenav = M.Sidenav.init(sidenav, {});
+
+  let dropdowns = document.querySelectorAll('.dropdown-trigger');
+  let instancia_dropwdown = M.Dropdown.init(dropdowns, {
+    hover: false
+  });
+
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems);
+
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+    duration: 400,
+    indicators: true
+  });
+
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, {
+    indicators: false,
+    interval: 4000,
+    height: 500
+  });
+});
+
         
-      document.addEventListener('DOMContentLoaded', function() {
-        let sidenav = document.querySelectorAll('.sidenav');
-        let instancia_sidenav = M.Sidenav.init(sidenav,{});    
-        let dropdowns = document.querySelectorAll('.dropdown-trigger');
-        let instancia_dropwdown = M.Dropdown.init(dropdowns, {
-        hover:false});
-      });
-      document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.carousel');
-        var instances = M.Carousel.init(elems,{
-            duration:400,
-            indicators:true
-        });
-      });
-      document.addEventListener('DOMContentLoaded', function() {
-        let sidenav = document.querySelectorAll('.sidenav');
-        let instancia_sidenav = M.Sidenav.init(sidenav,{});    
-        let dropdowns = document.querySelectorAll('.dropdown-trigger');
-        let instancia_dropwdown = M.Dropdown.init(dropdowns, {
-        hover:false});
-        });
-    
+      
+     
